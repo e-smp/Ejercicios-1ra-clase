f47af38e7557659819cc7695b1c8b83267bada5c
e-smp <e.sanmartinpez@gmail.com>
2024-03-06 21:26:52 -0300

    agregué el enunciado del ejercicio

---
+++ primerEjercicio.py
@@ -0,0 +1,16 @@
+"""Un teatro otorga descuentos según la edad del cliente. Determinar la cantidad de dinero que el teatro
+deja de percibir por cada una de las categorías. Tomar en cuenta que los niños menores de 5 años
+no pueden entrar al teatro y que existe un precio único en los asientos. Los descuentos se hacen
+tomando en cuenta el siguiente cuadro:
+Edad Descuento
+ Categoría 1 5 - 14 35 %
+ Categoría 2 15 - 19 25 %
+ Categoría 3 20 - 45 10 %
+ Categoría 4 46 - 65 25 %
+ Categoría 5 66 en adelante 35 %
+Desarrollar haciendo uso de:
+a.1) Usar estructura condicional/selección simple
+a.2) Usar estructura condicional/selección doble
+Ejercicios a desarrollar
+"""
+
